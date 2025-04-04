230801b49a7d4d1c15501f5849076bdc6efb5174
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2025-04-04 21:56:44 +0530

    Update npmInstall.groovy

--- vars/npmInstall.groovy
+++ vars/npmInstall.groovy
@@ -4,9 +4,8 @@ def call() {
         sh '''
             if ! command -v npm &> /dev/null
             then
-                echo "npm is not installed. Installing Node.js and npm..."
-                curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
-                sudo apt-get install -y nodejs
+                echo "❌ npm is missing. Please install Node.js manually."
+                exit 1
             fi
             npm install
         '''
